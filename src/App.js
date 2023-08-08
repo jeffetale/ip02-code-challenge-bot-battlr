d21e8b855d45a3bd830d110a1ff619645e23f88e
jeff <jeff.etale@student.moringaschool.com>
2023-08-08 09:49:48 +0300

    added more styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import BotList from './Components/BotList';
-import BotFavourite from './Components/BotFavourite';
-import BotSpecs from './Components/BotSpecs';
+import React, { useState, useEffect } from "react";
+import BotList from "./Components/BotList";
+import BotFavourite from "./Components/BotFavourite";
+import BotSpecs from "./Components/BotSpecs";
+import { Container, Row, Col, Card } from "react-bootstrap";
 
 function App() {
   const [bots, setBots] = useState([]);
@@ -9,7 +10,7 @@ function App() {
   const [selectedBot, setSelectedBot] = useState(null);
 
   useEffect(() => {
-    fetch('https://trendsserver.onrender.com/bots')
+    fetch("https://trendsserver.onrender.com/bots")
       .then((response) => response.json())
       .then((data) => setBots(data));
   }, []);
@@ -33,15 +34,37 @@ function App() {
   }
 
   return (
-    <div>
-      {selectedBot ? (
-        <BotSpecs bot={selectedBot} onEnlist={() => handleAddToFavourites(selectedBot)} onBack={handleBack} />
-      ) : (
-        <BotList bots={bots} onSelect={handleSelectBot} onAddToFavourites={handleAddToFavourites} />
-      )}
-      <BotFavourite favouriteBots={favouriteBots} onRemoveFromFavourites={handleRemoveFromFavourites} />
-    </div>
+    <Container>
+      <Row>
+        <Col md={8}>
+          {selectedBot ? (
+            <BotSpecs
+              bot={selectedBot}
+              onEnlist={() => handleAddToFavourites(selectedBot)}
+              onBack={handleBack}
+            />
+          ) : (
+            <>
+           
+              <Card className="mb-4">
+                <BotList
+                  bots={bots}
+                  onSelect={handleSelectBot}
+                  onAddToFavourites={handleAddToFavourites}
+                />
+              </Card>
+              
+              <Card>
+                <BotFavourite
+                  favouriteBots={favouriteBots}
+                  onRemoveFromFavourites={handleRemoveFromFavourites}
+                />
+              </Card>
+            </>
+          )}
+        </Col>
+      </Row>
+    </Container>
   );
 }
-
 export default App;
